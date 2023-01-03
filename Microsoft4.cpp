5fde92cc5dc147b8e0898892b484329320302e5f
Rajneesh Prasad <120679431+rajneeshr21@users.noreply.github.com>
2023-01-03 12:02:18 +0530

    Microsoft Q4

---
+++ Microsoft4.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxRotateFunction(vector<int>& nums) {
+        int curr=0,maxi,n=nums.size(),sum=0;
+        for(int i=0;i<n;i++){
+            sum+=nums[i];
+            curr+=i*nums[i];
+        }
+        maxi=curr;
+        for(int i=n-1;i>=0;i--){
+            curr=curr+(sum-nums[i])-(n-1)*nums[i];
+            maxi=max(maxi,curr);
+        }
+        return maxi;
+    }
+};
